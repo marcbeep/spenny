a68a71814a2f5dc2b8f78c9f2df5d11744e41f01
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-01-31 22:25:41 +0000

    fix: CORS on user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const { signupUser, loginUser } = require('../controllers/userController');
+
+const allowedOrigins = ['https://spenny.reeflink.org', 'http://localhost:3000'];
+
+router.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      const message =
+        'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(message), false);
+    },
+  })
+);
 
 router.post('/login', loginUser);
 router.post('/signup', signupUser);
