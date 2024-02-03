f8d27d8e06b15a1b238cb13e9fee5868e7a34be4
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-02-03 19:32:11 +0000

    fix: api url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const backendURL = process.env.NODE_ENV === 'production'
-  ? 'https://spenny-api.reeflink.org'
+  ? 'https://spenny-6e54c38e0b23.herokuapp.com'
   : 'http://localhost:3000'; 
 
 export default backendURL;
