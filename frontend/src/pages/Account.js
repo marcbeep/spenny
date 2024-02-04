d5a1b1d89681ff6a2a1ebf0c1955897e584d47c1
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-02-04 16:44:10 +0000

    feat: display accounts

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useAccountContext } from '../context/AccountContext'; // Ensure this import is correct
 import backendURL from '../config';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const Account = () => {
   const { user } = useAuthContext();
   const { accounts, dispatch: dispatchAccount } = useAccountContext(); // Correctly extract dispatchAccount here
   const [isLoading, setIsLoading] = useState(false);
+  const colors = ['bg-red-400', 'bg-blue-400', 'bg-green-400', 'bg-yellow-400', 'bg-purple-400'];
 
   useEffect(() => {
     const fetchAccounts = async () => {
@@ -40,17 +43,32 @@ const Account = () => {
   }
 
   return (
-    <div>
+    <>
+    <h1 className='font-semibold'>Accounts</h1>
+    <div className="flex justify-end">
+          <button className='btn btn-primary mb-4' onClick={console.log("hello")}>
+            <FontAwesomeIcon icon={faPlus} size='sm' />
+          </button>
+      </div>
+    <div className="flex flex-wrap justify-center gap-4 p-4 text-base-100">
       {accounts.length > 0 ? (
-        <ul>
-          {accounts.map(account => (
-            <li key={account.id}>{account.name}</li>
-          ))}
-        </ul>
+        accounts.map((account, index) => (
+          <div
+            key={account.id}
+            className={`card rounded-xl w-48 ${colors[index % colors.length]} lg:w-1/4 md:w-1/2 sm:w-full`}
+          >
+            <div className="card-body">
+              <h2 className="card-title">{account.name}</h2>
+              <p>Balance: £{account.balance}</p>
+              <div className="badge badge-outline">{account.type}</div>
+            </div>
+          </div>
+        ))
       ) : (
         <div>No accounts available</div>
       )}
     </div>
+    </>
   );
 };
 
