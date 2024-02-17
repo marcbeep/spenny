6cddcfcf782cf261b26478b1ad0e36dde043ad22
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-02-17 09:21:25 +0000

    style: font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inconsolata', 'sans'],
+        sans: ['Inter', 'sans'],
       },
       fontWeight: {
         light: 200,
