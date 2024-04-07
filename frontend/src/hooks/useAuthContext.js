5a814aee5aced4399877c27993757ace0f47e3e3
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-04-07 21:22:08 +0100

    category

--- frontend/src/hooks/useAuthContext.js
+++
@@ -1,8 +0,0 @@
-import { AuthContext } from '../context/AuthContext';
-import { useContext } from 'react';
-
-export const useAuthContext = () => {
-  const { user, dispatch } = useContext(AuthContext);
-
-  return { user, dispatch };
-};
