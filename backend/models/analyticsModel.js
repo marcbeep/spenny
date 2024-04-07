cca855695b275a5b93250d2f6851e2226ec9dfcd
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-04-07 12:11:30 +0100

    feat: analytics schema

---
+++ backend/models/analyticsModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Helper function for formatting numbers to two decimal places
+function formatNumber(value) {
+  return parseFloat(parseFloat(value).toFixed(2));
+}
+
+// Subdocument schema for analytics data entries
+const analyticsEntrySchema = new Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  value: {
+    type: Number,
+    required: true,
+    set: formatNumber, 
+  }
+}, { _id: false }); // Disable _id for subdocument
+
+const analyticsSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  analyticsType: {
+    type: String,
+    required: true,
+    lowercase: true, 
+    enum: ['networth', 'spending'], // Restricts the value to 'networth' or 'spending'
+  },
+  analyticsData: [analyticsEntrySchema], // Use the subdocument schema for entries
+  analyticsLastCalculated: {
+    type: Date,
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Analytics', analyticsSchema);
