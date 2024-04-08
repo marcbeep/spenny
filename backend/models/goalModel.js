6b3e6e359f692481836d874cecaa1c649f2f0b25
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-04-08 06:28:47 +0100

    fix: Goal Schema to conform to New Specs

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
 // Helper function for formatting numbers to two decimal places
@@ -7,46 +6,47 @@ function formatNumber(value) {
   return parseFloat(parseFloat(value).toFixed(2));
 }
 
-const goalSchema = new Schema(
-  {
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    goalCategory: {
-      type: Schema.Types.ObjectId,
-      ref: 'Category',
-      required: true,
-    },
-    goalType: {
-      type: String,
-      required: true,
-      lowercase: true,
-      enum: ['spending', 'saving', 'minimumbalance'],
-    },
-    goalTarget: {
-      type: Number,
-      required: false, // Not required for 'minimum' goal type
-      set: formatNumber,
-    },
-    goalDeadline: {
-      type: Date,
-      required: false, // Only required for 'spending' goal type
-    },
-    goalStatus: {
-      type: String,
-      required: true,
-      lowercase: true,
-      enum: ['underfunded', 'funded'],
-    },
-    goalResetDay: {
-      type: String,
-      enum: ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'],
-      default: null,
-    },
+const goalSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
   },
-  { timestamps: true },
-);
+  goalCategory: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  goalType: {
+    type: String,
+    required: true,
+    lowercase: true,
+    enum: ['spending', 'saving', 'minimumbalance'], // Define allowed goal types
+  },
+  goalTarget: {
+    type: Number,
+    required: function() {
+      // Required only for 'spending' and 'saving' goal types
+      return this.goalType === 'spending' || this.goalType === 'saving';
+    },
+    set: formatNumber,
+  },
+  // Removed goalDeadline as it's not used in the new specs
+  goalStatus: {
+    type: String,
+    required: true,
+    lowercase: true,
+    enum: ['underfunded', 'funded'], // Status of the goal
+  },
+  goalResetDay: {
+    type: String,
+    enum: ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'],
+    required: function() {
+      // Required only for 'spending' goal type
+      return this.goalType === 'spending';
+    },
+    default: null, // Specifies the day the 'spending' goal resets
+  },
+}, { timestamps: true });
 
 module.exports = mongoose.model('Goal', goalSchema);
