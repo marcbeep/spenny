2ac582c73b1885c0f61719057241134e93992c7e
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-04-09 14:05:04 +0100

    chore: clean utils

--- backend/utils/checkAndUpdateGoalStatus.js
+++ backend/utils/checkAndUpdateGoalStatus.js
@@ -1,50 +1,48 @@
 const Goal = require('../models/goalModel');
-const Category = require('../models/categoryModel');
 const moment = require('moment');
 
 const checkAndUpdateGoalStatus = async (goalId = null, categoryId = null) => {
-  let goals;
+  try {
+    let goals;
 
-  // If a goalId is provided, find the specific goal
-  if (goalId) {
-    goals = await Goal.find({ _id: goalId }).populate('goalCategory');
-  } else if (categoryId) {
-    // If a categoryId is provided, find all goals associated with that category
-    goals = await Goal.find({ goalCategory: categoryId }).populate('goalCategory');
-  } else {
-    // If neither is provided, fetch all goals
-    goals = await Goal.find().populate('goalCategory');
-  }
-
-  const processGoal = async (goal) => {
-    let isFunded = false;
-
-    switch (goal.goalType) {
-      case 'saving':
-        isFunded = goal.goalTarget <= goal.goalCategory.categoryAvailable;
-        break;
-      case 'spending':
-        const today = moment();
-        const resetDay = moment().isoWeekday(goal.goalResetDay);
-        const resetDayPassed = today.isSameOrAfter(resetDay, 'day');
-
-        if (resetDayPassed) {
-          isFunded = false;
-        } else {
-          isFunded = goal.goalTarget <= goal.goalCategory.categoryAvailable;
-        }
-        break;
-      default:
-        console.error(`Unknown goal type: ${goal.goalType}`);
+    if (goalId) {
+      goals = await Goal.find({ _id: goalId }).populate('goalCategory');
+    } else if (categoryId) {
+      goals = await Goal.find({ goalCategory: categoryId }).populate('goalCategory');
+    } else {
+      goals = await Goal.find().populate('goalCategory');
     }
 
-    goal.goalStatus = isFunded ? 'funded' : 'underfunded';
-    await goal.save();
-  };
+    const processGoal = async (goal) => {
+      let isGoalFunded = false;
 
-  // Process each goal found
-  for (let goal of goals) {
-    await processGoal(goal);
+      switch (goal.goalType) {
+        case 'saving':
+          isGoalFunded = goal.goalTarget <= goal.goalCategory.categoryAvailable;
+          break;
+        case 'spending':
+          const today = moment();
+          const resetDay = moment().isoWeekday(goal.goalResetDay);
+          const resetDayPassed = today.isSameOrAfter(resetDay, 'day');
+
+          if (!resetDayPassed) {
+            isGoalFunded = goal.goalTarget <= goal.goalCategory.categoryAvailable;
+          }
+          break;
+        default:
+          console.error(`Unknown goal type: ${goal.goalType}`);
+      }
+
+      goal.goalStatus = isGoalFunded ? 'funded' : 'underfunded';
+      await goal.save();
+    };
+
+    for (let goal of goals) {
+      await processGoal(goal);
+    }
+  } catch (error) {
+    console.error('Error in checkAndUpdateGoalStatus:', error);
+    throw error; // Propagate the error to the caller
   }
 };
 
--- backend/utils/updateAllUserAnalytics.js
+++ backend/utils/updateAllUserAnalytics.js
@@ -1,21 +1,29 @@
-const User = require('../models/userModel'); // Assuming you have a User model
-const analyticsServices = require('../jobs/analyticsServices'); // Path to your refactored analytics services
+const User = require('../models/userModel');
+const analyticsServices = require('../jobs/analyticsServices');
 
 async function updateAllUserAnalytics() {
-  const users = await User.find({}); // Fetch all user IDs
-  for (const user of users) {
-    try {
-      await analyticsServices.calculateTotalSpendForUserId(user._id);
-      await analyticsServices.calculateSpendingByCategoryForUserId(user._id);
-      await analyticsServices.calculateNetWorthForUserId(user._id);
-      await analyticsServices.calculateIncomeVsExpensesForUserId(user._id);
-      await analyticsServices.calculateSavingsRateForUserId(user._id);
-      await analyticsServices.calculateAllTimeAnalyticsForUserId(user._id);
-      console.log(`Analytics updated for user ${user._id}`);
-    } catch (error) {
-      console.error(`Error updating analytics for user ${user._id}:`, error);
+  try {
+    const users = await User.find({});
+
+    for (const user of users) {
+      try {
+        await analyticsServices.calculateTotalSpendForUserId(user._id);
+        await analyticsServices.calculateSpendingByCategoryForUserId(user._id);
+        await analyticsServices.calculateNetWorthForUserId(user._id);
+        await analyticsServices.calculateIncomeVsExpensesForUserId(user._id);
+        await analyticsServices.calculateSavingsRateForUserId(user._id);
+        await analyticsServices.calculateAllTimeAnalyticsForUserId(user._id);
+        console.log(`Analytics updated for user ${user._id}`);
+      } catch (error) {
+        console.error(`Error updating analytics for user ${user._id}:`, error);
+      }
     }
+
+    console.log('All user analytics updated successfully');
+  } catch (error) {
+    console.error('Error fetching users:', error);
   }
 }
 
 module.exports = { updateAllUserAnalytics };
+
