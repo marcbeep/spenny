e17f3c0efc3f38a6f5a33e7174be657a636ac53c
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-04-10 16:16:34 +0100

    new navbar

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -14,17 +14,31 @@ const Navbar = () => {
   };
 
   return (
-    <div className={`drawer drawer-left ${isDrawerOpen ? 'active' : ''}`}>
+    <div className={`navbar bg-base-100 drawer drawer-left ${isDrawerOpen ? 'active' : ''}`}>
       <input id="my-drawer" type="checkbox" className="drawer-toggle" checked={isDrawerOpen} readOnly />
       <div className='drawer-content flex flex-col'>
         {/* Navbar content */}
-        {user ? (
-          <button onClick={() => logout()} className='btn btn-ghost'>
-            Logout
-          </button>
-        ) : (
-          <div className='py-2 px-4'></div>
-        )}
+        <div className="flex-1">
+          <a className="btn btn-ghost text-2xl">spenny</a>
+        </div>
+        <div className="flex-none">
+          {user && (
+            <div className="dropdown dropdown-end">
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img alt="User Avatar" src={user.avatar} />
+                </div>
+              </div>
+              <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <li>
+                  <button onClick={() => logout()} className="justify-between">
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
       <div className='drawer-side'>
         <label htmlFor="my-drawer" className="drawer-overlay" aria-label="close sidebar" onClick={closeDrawer}></label>
@@ -38,4 +52,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
