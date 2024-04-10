d36c6aa51630a86f504c61550db471b77ba31f0b
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-04-10 13:00:21 +0100

    accounts

---
+++ frontend/src/components/Forms/OCRUpload.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import Tesseract from 'tesseract.js';
+import { analyzeTextWithGPT } from '../../utils/analyzeTextWithGPT'; // Adjust the path as necessary
+
+const OCRUpload = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [ocrText, setOcrText] = useState('');
+  const [gptResult, setGptResult] = useState(''); // State to store GPT-3 analysis result
+
+  const handleImageUpload = (event) => {
+    const imageFile = event.target.files[0];
+    if (!imageFile) {
+      console.log('No file selected.');
+      return;
+    }
+
+    setIsLoading(true);
+    setOcrText(''); // Reset OCR text for new uploads
+    setGptResult(''); // Reset GPT result for new uploads
+    Tesseract.recognize(imageFile, 'eng', {
+      logger: (m) => console.log(m),
+    }).then(({ data: { text } }) => {
+      console.log(text);
+      setOcrText(text); // Update state with OCR text
+      // Use text directly for GPT analysis
+      const gptPrompt = `Given the following OCR text extracted from a receipt, give your best guess to fill out the following fields: success (either true or false), transaction_title, amount, category, type (credit or debit). If you can guess, return success: true followed by the rest of fields. If you cannot make an intelligent guess, return success:false in json only. You can generalise for the transaction_title. For example, "Paul's Italiano" might mean "Italian Food". Here is the text: ${text}`;
+      analyzeTextWithGPT(gptPrompt)
+        .then((result) => {
+          console.log(result);
+          setGptResult(result); // Update state with GPT analysis result
+          setIsLoading(false); // Update loading state
+        })
+        .catch((error) => {
+          console.error('Error analyzing text with GPT:', error);
+          setIsLoading(false); // Ensure loading state is updated in case of error
+        });
+    });
+  };
+
+  return (
+    <div className='card bg-base-100 shadow-xl p-8'>
+      <div className='form-control'>
+        <label className='label'>
+          <span className='label-text'>Upload a clear picture of your receipt</span>
+        </label>
+        <input
+          type='file'
+          accept='image/*'
+          onChange={handleImageUpload}
+          className='file-input file-input-bordered w-full max-w-xs'
+        />
+        {isLoading && (
+          <div className='flex justify-left items-left mt-4'>
+            <progress className='progress w-56'></progress>
+          </div>
+        )}
+        {ocrText && (
+          <div className='mt-4'>
+            <div className='label'>
+              <span className='label-text'>Extracted Text</span>
+            </div>
+            <p>{ocrText}</p>
+          </div>
+        )}
+        {gptResult && (
+          <div className='mt-4'>
+            <div className='label'>
+              <span className='label-text'>GPT-3.5 Analysis</span>
+            </div>
+            <p>{gptResult}</p> {/* Display the GPT-3.5 analysis result */}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default OCRUpload;
---
+++ frontend/src/components/Layouts/Navbar.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useLogout } from '../../hooks/useLogout';
+
+const Navbar = () => {
+  const { user } = useAuthContext();
+  const location = useLocation();
+  const logout = useLogout();
+  const isActive = (path) => (location.pathname === path ? 'font-bold' : '');
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const closeDrawer = () => {
+    setIsDrawerOpen(false);
+  };
+
+  return (
+    <div className={`drawer drawer-left ${isDrawerOpen ? 'active' : ''}`}>
+      <input
+        id='my-drawer'
+        type='checkbox'
+        className='drawer-toggle'
+        checked={isDrawerOpen}
+        readOnly
+      />
+      <div className='drawer-content flex flex-col'>
+        <header className='bg-primary text-primary-content px-8 py-8 flex justify-between items-center'>
+          {user && (
+            <button onClick={toggleDrawer} className='btn btn-ghost'>
+              <span className='text-xl'>☰</span> {/* You can also use an icon here */}
+            </button>
+          )}
+
+          <div className='flex-grow'>
+            <Link to='/' className='text-3xl font-bold ml-4'>
+              spenny
+            </Link>
+          </div>
+
+          {user ? (
+            <button onClick={logout} className='btn btn-ghost'>
+              Logout
+            </button>
+          ) : (
+            <div className='py-2 px-4'></div>
+          )}
+        </header>
+      </div>
+      <div className='drawer-side'>
+        <label
+          htmlFor='my-drawer'
+          className='drawer-overlay'
+          aria-label='close sidebar'
+          onClick={closeDrawer}
+        ></label>
+        <ul className='menu py-32 overflow-y-auto w-80 h-full bg-primary text-primary-content'>
+          <li>
+            <Link
+              to='/transaction'
+              className={`p-2 ${isActive('/transaction')}`}
+              onClick={closeDrawer}
+            >
+              Transactions
+            </Link>
+          </li>
+          <li>
+            <Link to='/account' className={`p-2 ${isActive('/account')}`} onClick={closeDrawer}>
+              Accounts
+            </Link>
+          </li>
+          <li>
+            <Link to='/category' className={`p-2 ${isActive('/category')}`} onClick={closeDrawer}>
+              Categories
+            </Link>
+          </li>
+          {/* <li>
+            <Link to='/dash' className={`p-2 ${isActive('/dash')}`} onClick={closeDrawer}>
+              Dash
+            </Link>
+          </li> */}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
