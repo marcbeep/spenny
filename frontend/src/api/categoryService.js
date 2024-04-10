8c09cc185a2be3ad8c3eaacbf1c5bf4d8fd1055d
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-04-10 13:07:42 +0100

    categories

---
+++ frontend/src/api/categoryService.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import backendURL from '../config';
+
+const API_BASE_URL = backendURL;
+
+// Set up Axios for Authorization header globally or per request based on user's token.
+axios.defaults.headers.common['Authorization'] = '';
+
+export const getCategories = async () => {
+  const response = await axios.get(`${API_BASE_URL}/categories`);
+  return response.data;
+};
+
+export const getCategoryById = async (categoryId) => {
+  const response = await axios.get(`${API_BASE_URL}/categories/${categoryId}`);
+  return response.data;
+};
+
+export const addCategory = async (categoryData) => {
+  const response = await axios.post(`${API_BASE_URL}/categories`, categoryData);
+  return response.data;
+};
+
+export const updateCategory = async (categoryId, updateData) => {
+  // updateData should contain the new title or other fields to be updated
+  const response = await axios.patch(`${API_BASE_URL}/categories/${categoryId}`, updateData);
+  return response.data;
+};
+
+export const deleteCategory = async (categoryId) => {
+  const response = await axios.delete(`${API_BASE_URL}/categories/${categoryId}`);
+  return response.data; 
+};
