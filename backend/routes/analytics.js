c1b645db8e01a78adca9be19b448064155fedb8b
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-04-12 16:34:10 +0100

    rm nw graph

--- backend/routes/analytics.js
+++ backend/routes/analytics.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const {
-  networthPastWeek,
   outgoingsPastWeek,
   spendByCategoryPastWeek,
   allTimeAnalytics,
@@ -10,7 +9,6 @@ const requireAuth = require('../middleware/requireAuth');
 const router = express.Router();
 router.use(requireAuth);
 
-router.get('/networthPastWeek', networthPastWeek);
 router.get('/outgoingsPastWeek', outgoingsPastWeek);
 router.get('/spendByCategoryPastWeek', spendByCategoryPastWeek);
 router.get('/allTimeAnalytics', allTimeAnalytics);
