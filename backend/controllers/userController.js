c32ffa04667eddb2f789ce16fc6aaa4179bc4ae7
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-04-14 22:25:01 +0100

    dash

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,11 +2,10 @@ const User = require('../models/userModel');
 const Account = require('../models/accountModel');
 const Category = require('../models/categoryModel');
 const Budget = require('../models/budgetModel');
-const Goal = require('../models/goalModel'); 
+const Goal = require('../models/goalModel');
 const Transaction = require('../models/transactionModel');
 const moment = require('moment');
 const jwt = require('jsonwebtoken');
-
 
 const createToken = (_id) => jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' });
 
@@ -18,25 +17,34 @@ const initializeUserData = async (userId) => {
       { accountTitle: 'Monzo', accountType: 'spending', accountBalance: 30 },
       { accountTitle: 'Vanguard Index Funds', accountType: 'tracking', accountBalance: 100 },
     ];
-    
+
     const accounts = await Promise.all(
-      accountsData.map(accountData => Account.create({
-        user: userId,
-        ...accountData,
-      }))
+      accountsData.map((accountData) =>
+        Account.create({
+          user: userId,
+          ...accountData,
+        }),
+      ),
     );
 
     // Create categories
-    const genericCategories = ['groceries', 'transport', 'eating out', 'fun money', 'savings', 'rent'];
+    const genericCategories = [
+      'groceries',
+      'transport',
+      'eating out',
+      'fun money',
+      'savings',
+      'rent',
+    ];
     const categories = await Promise.all(
-      genericCategories.map(categoryTitle =>
+      genericCategories.map((categoryTitle) =>
         Category.create({
           user: userId,
           categoryTitle,
           categoryAvailable: 10.0,
           categoryActivity: 0.0,
-        })
-      )
+        }),
+      ),
     );
 
     // Create a budget
@@ -66,7 +74,10 @@ const updateCategoryActivity = async (userId, categories) => {
 
   // Map of category IDs to their total activity
   const activityMap = transactions.reduce((acc, transaction) => {
-    const amount = transaction.transactionType === 'debit' ? -transaction.transactionAmount : transaction.transactionAmount;
+    const amount =
+      transaction.transactionType === 'debit'
+        ? -transaction.transactionAmount
+        : transaction.transactionAmount;
     if (acc[transaction.transactionCategory]) {
       acc[transaction.transactionCategory] += amount;
     } else {
@@ -76,7 +87,7 @@ const updateCategoryActivity = async (userId, categories) => {
   }, {});
 
   // Update each category with the calculated activity
-  const updatePromises = categories.map(category => {
+  const updatePromises = categories.map((category) => {
     const activity = activityMap[category._id.toString()] || 0;
     return Category.findByIdAndUpdate(category._id, { $inc: { categoryActivity: activity } });
   });
@@ -87,93 +98,94 @@ const updateCategoryActivity = async (userId, categories) => {
 // Example Transactions Creation Function
 const createExampleTransactions = async (userId, categories, accounts) => {
   // Select spending accounts
-  const spendingAccounts = accounts.filter(account => account.accountType === 'spending');
+  const spendingAccounts = accounts.filter((account) => account.accountType === 'spending');
 
   // Example transaction details
   const transactionsData = [
     {
       transactionTitle: 'transfer from liam for uber',
       transactionType: 'credit',
-      transactionAmount: 1.00,
+      transactionAmount: 1.0,
       transactionDate: moment().toDate(),
-      transactionCategory: categories.find(c => c.categoryTitle === 'transport'), 
-      transactionAccount: spendingAccounts[1]
+      transactionCategory: categories.find((c) => c.categoryTitle === 'transport'),
+      transactionAccount: spendingAccounts[1],
     },
     {
       transactionTitle: 'asda shopping',
       transactionType: 'debit',
-      transactionAmount: 1.00,
+      transactionAmount: 1.0,
       transactionDate: moment().subtract(1, 'days').toDate(),
-      transactionCategory: categories.find(c => c.categoryTitle === 'groceries'),
-      transactionAccount: spendingAccounts[0]
+      transactionCategory: categories.find((c) => c.categoryTitle === 'groceries'),
+      transactionAccount: spendingAccounts[0],
     },
     {
       transactionTitle: 'drinks at spoons',
       transactionType: 'debit',
-      transactionAmount: 2.00,
+      transactionAmount: 2.0,
       transactionDate: moment().subtract(2, 'days').toDate(),
-      transactionCategory: categories.find(c => c.categoryTitle === 'fun money'),
-      transactionAccount: spendingAccounts[0]
+      transactionCategory: categories.find((c) => c.categoryTitle === 'fun money'),
+      transactionAccount: spendingAccounts[0],
     },
     {
       transactionTitle: 'fix door handles',
       transactionType: 'debit',
-      transactionAmount: 3.00,
+      transactionAmount: 3.0,
       transactionDate: moment().subtract(3, 'days').toDate(),
-      transactionCategory: categories.find(c => c.categoryTitle === 'rent'), 
-      transactionAccount: spendingAccounts[1]
+      transactionCategory: categories.find((c) => c.categoryTitle === 'rent'),
+      transactionAccount: spendingAccounts[1],
     },
     {
       transactionTitle: 'kfc dinner',
       transactionType: 'debit',
-      transactionAmount: 4.00,
+      transactionAmount: 4.0,
       transactionDate: moment().subtract(4, 'days').toDate(),
-      transactionCategory: categories.find(c => c.categoryTitle === 'eating out'),
-      transactionAccount: spendingAccounts[0]
+      transactionCategory: categories.find((c) => c.categoryTitle === 'eating out'),
+      transactionAccount: spendingAccounts[0],
     },
     {
       transactionTitle: 'milos dog food',
       transactionType: 'debit',
-      transactionAmount: 5.00,
+      transactionAmount: 5.0,
       transactionDate: moment().subtract(5, 'days').toDate(),
-      transactionCategory: categories.find(c => c.categoryTitle === 'eating out'),
-      transactionAccount: spendingAccounts[1]
+      transactionCategory: categories.find((c) => c.categoryTitle === 'eating out'),
+      transactionAccount: spendingAccounts[1],
     },
     {
       transactionTitle: 'maccies breakfast',
       transactionType: 'debit',
-      transactionAmount: 6.00,
+      transactionAmount: 6.0,
       transactionDate: moment().subtract(6, 'days').toDate(),
-      transactionCategory: categories.find(c => c.categoryTitle === 'eating out'),
-      transactionAccount: spendingAccounts[1]
+      transactionCategory: categories.find((c) => c.categoryTitle === 'eating out'),
+      transactionAccount: spendingAccounts[1],
     },
     {
       transactionTitle: 'lidl bakery item',
       transactionType: 'debit',
-      transactionAmount: 7.00,
+      transactionAmount: 7.0,
       transactionDate: moment().subtract(7, 'days').toDate(),
-      transactionCategory: categories.find(c => c.categoryTitle === 'groceries'),
-      transactionAccount: spendingAccounts[1]
-    }
+      transactionCategory: categories.find((c) => c.categoryTitle === 'groceries'),
+      transactionAccount: spendingAccounts[1],
+    },
   ];
 
   // Create transaction records
   await Promise.all(
-    transactionsData.map(transaction =>
+    transactionsData.map((transaction) =>
       Transaction.create({
         user: userId,
-        transactionCategory: transaction.transactionCategory ? transaction.transactionCategory._id : null,
+        transactionCategory: transaction.transactionCategory
+          ? transaction.transactionCategory._id
+          : null,
         transactionAccount: transaction.transactionAccount._id,
         transactionType: transaction.transactionType,
         transactionTitle: transaction.transactionTitle,
         transactionAmount: transaction.transactionAmount,
         createdAt: transaction.transactionDate,
         updatedAt: transaction.transactionDate,
-      })
-    )
+      }),
+    ),
   );
 };
-
 
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
